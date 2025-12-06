8c503db66af8296a839a4f7aa10805b7a62ddbbc
Tharunoff <s.tharunsofficial@gmail.com>
2025-12-06 17:09:10 +0530

    Delete all files from remote repo

--- src/homepage/types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Course {
-    id: number;
-    title: string;
-    author: string;
-    date: string;
-    image: string;
-}
-
-export type TabType = 'nptel' | 'gate' | 'srm' | 'global';
