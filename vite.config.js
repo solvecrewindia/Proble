040c3377b12e5afac8b985670b5e27aed18522fc
Tharunoff <s.tharunsofficial@gmail.com>
2025-12-29 23:57:38 +0530

    Update PWA icons and status bar color

--- vite.config.js
+++ vite.config.js
@@ -16,22 +16,17 @@ export default defineConfig({
         name: 'Proble',
         short_name: 'Proble',
         description: 'Proble Learning Platform',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
+        theme_color: '#000000',
+        background_color: '#000000',
         display: 'standalone',
         icons: [
           {
-            src: 'favicon.png',
-            sizes: '64x64 32x32 24x24 16x16',
-            type: 'image/png'
-          },
-          {
-            src: 'logo-dark.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'logo-dark.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
